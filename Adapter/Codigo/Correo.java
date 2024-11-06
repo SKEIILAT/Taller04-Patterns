d0dc0ebfedc490df5313fba88c66a7a67881bdde
BrayanBriones <137122666+BrayanBriones@users.noreply.github.com>
2024-11-06 17:05:32 -0500

    Update Correo.java

--- Adapter/Codigo/Correo.java
+++ Adapter/Codigo/Correo.java
@@ -1,5 +1,5 @@
 public class Correo {
-    public void enviarNotificacion(String mensaje) {
-        System.out.println("Enviando correo con mensaje: " + mensaje);
+    public void enviarNotificacion() {
+        System.out.println("Enviando correo con mensaje: ");
     }
 }
