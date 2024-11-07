e7943982d8a56cd36b35d24c5aa895a798aa8f99
JairPalaguachi <jpalagua@espol.edu.ec>
2024-11-06 22:18:54 -0500

    Create Correo.java

---
+++ FactoryMethod/Correo.java
@@ -0,0 +1,12 @@
+package factorymethod;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Correo extends Notificacion {
+    
+    public void enviarNotificacion(){
+        System.out.println("Enviando notificacion al correo electronico...");
+    }
+}
